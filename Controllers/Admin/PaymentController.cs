9118d14a3a4082af94bad1aca6256c341de26e56
Ravi Technical <129148887+Ravi-Technical@users.noreply.github.com>
2026-01-31 09:38:51 +0530

    Remove comment in payment controller

--- Controllers/Admin/PaymentController.cs
+++ Controllers/Admin/PaymentController.cs
@@ -14,7 +14,7 @@ namespace Udemy_Backend.Controllers.Admin
              paymentService= paymentInterface;
         }
 
-        //**************** PAYMENT CREATE ENDPOINT testing ******************//
+        //**************** PAYMENT CREATE ENDPOINT ******************//
         [HttpPost("createOrder")]
         public async Task<IActionResult> CreateNewOrder(CreateOrderRequest req)
         {
